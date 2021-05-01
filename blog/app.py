5f16fc58e7b7f27713cf57dcaf6703e06df1e24f
formika92 <formika92@gmail.com>
2021-05-01 11:04:23 -0700

    Added files for HW 1

---
+++ blog/app.py
@@ -0,0 +1,11 @@
+from flask import (
+    Flask,
+    request
+)
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return f'This is a {request.method} '
---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from blog.app import app
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=True,
+    )
+
+
+def util():
+    pass
